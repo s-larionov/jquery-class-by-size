fad3e624accf9103f3ec16e469bdf3d614343e85
Sergey Larionov <sergey@larionov.biz>
2012-07-19 17:16:15 +1100

    Сделал возможность указать общий параметр applyTo. Теперь можно вызывать так: $(...).ClassBySize([... rules ...]); или так (applyTo необязательный): $(...).ClassBySize({rules: [... rules ...], applyTo: ...});

--- jquery-classbysize.js
+++ jquery-classbysize.js
@@ -1,46 +1,62 @@
 (function($) { // secure $ jQuery alias
-/*******************************************************************************************/
+	/*******************************************************************************************/
 // jquery.classbysize.js - rev 5
 // Copyright (c) 2012, Larry (http://larionov.biz)
 // Liscensed under the BSD License (BSD-LICENSE.txt)
 // http://www.opensource.org/licenses/bsd-license.php
 // @created: 2011-01-18
 // @modified: 2012-07-19
-/*******************************************************************************************/
+	/*******************************************************************************************/
 
-$.ClassBySize = function(element, config) {
-	var self = this;
+	/**
+	 * @param {jQuery|DOMElement} element
+	 * @param {Array|Object} config
+	 * @constructor
+	 */
+	$.ClassBySize = function(element, config) {
+		var self = this;
 
-	this.config = config instanceof Array? config: [];
-	for (var i = 0, len = this.config.length; i < len; i++) {
-		this.config[i].applyTo = this.config[i].applyTo? $(this.config[i].applyTo): this.element;
-	}
+		this.element = $(element);
+		this.config = config && config.rules instanceof Array? config: (config instanceof Array? {rules: config}: {rules: []});
 
-	this.element = $(element);
+		if (!this.element.size() || !this.config.rules.length) {
+			return;
+		}
 
-	$(window).resize(function() {
-		self.resize.call(self);
-	});
+		this.applyTo = this.config.applyTo? $(this.config.applyTo): this.element;
+		for (var i = 0, len = this.config.rules.length; i < len; i++) {
+			this.config.rules[i].applyTo = this.config.rules[i].applyTo? $(this.config.rules[i].applyTo): this.applyTo;
+		}
 
-	this.resize();
-};
-$.ClassBySize.prototype.resize = function() {
-	var w = $.browser.opera && this.element.prop('innerWidth')? this.element.prop('innerWidth'): this.element.width(),
-		h = $.browser.opera && this.element.prop('innerHeight')? this.element.prop('innerHeight'): this.element.height();
+		$(window).resize(function() {
+			self.resize.call(self);
+		});
 
-	for (var i = 0, len = this.config.length; i < len; i++) {
-		var minW = this.config[i].minWidth || w,
-			maxW = this.config[i].maxWidth || (w + 1),
-			minH = this.config[i].minHeight || h,
-			maxH = this.config[i].maxHeight || (h + 1),
-			action = (w >= minW && w < maxW && h >= minH && h < maxH)? 'addClass': 'removeClass';
+		this.resize();
+	};
+	$.ClassBySize.prototype.resize = function() {
+		var w = $.browser.opera && this.element.prop('innerWidth')? this.element.prop('innerWidth'): this.element.width(),
+			h = $.browser.opera && this.element.prop('innerHeight')? this.element.prop('innerHeight'): this.element.height();
 
-		this.config[i].applyTo[action](this.config[i].className);
-	}
-};
-$.fn.ClassBySize = function(config) {
-	return this.each(function(i, element) {
-		new $.ClassBySize(element, config);
-	});
-};
+		for (var i = 0, len = this.config.rules.length; i < len; i++) {
+			var rule = this.config.rules[i];
+
+			if (!rule.className) {
+				continue;
+			}
+
+			var minW = rule.minWidth || w,
+				maxW = rule.maxWidth || (w + 1),
+				minH = rule.minHeight || h,
+				maxH = rule.maxHeight || (h + 1),
+				action = (w >= minW && w < maxW && h >= minH && h < maxH)? 'addClass': 'removeClass';
+
+			rule.applyTo[action](rule.className);
+		}
+	};
+	$.fn.ClassBySize = function(config) {
+		return this.each(function(i, element) {
+			new $.ClassBySize(element, config);
+		});
+	};
 })(jQuery);
